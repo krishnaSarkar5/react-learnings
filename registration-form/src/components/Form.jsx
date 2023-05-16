fa27d61f679176d8298d557bc761146bcdcaed38
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-05-16 23:32:18 +0530

    validation addded

--- registration-form/src/components/Form.jsx
+++ registration-form/src/components/Form.jsx
@@ -1,26 +1,87 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Form.css'
 import '../utils/Validation'
-import { validName } from '../utils/Validation';
+import { validName,validateEmail,validatePassword,validatePhone,validateConfirmPassword } from '../utils/Validation';
 
 const Form = () => {
 
-    const [firstName,setFirstName] = useState();
-    const [validNameState,setValidNameState] = useState(true);
-    const [lastName,setLastName] = useState();
-    const [countryCode,setCountryCode] = useState();
-    const [phone,setPhone] = useState();
-    const [email,setEmail] = useState();
-    const [password,setPassword] = useState();
-    const [confirmPassword,setConfirmPassword] = useState();
+    const [firstName,setFirstName] = useState("");
+    const [validFirstName,setValidFirstName] = useState(false);
+
+    const [lastName,setLastName] = useState("");
+    const [validLastName,setValidLastName] = useState(false);
+
+
+    const [countryCode,setCountryCode] = useState("");
+
+    const [phone,setPhone] = useState("");
+    const [validPhone,setValidPhone] = useState(false);
+
+    const [email,setEmail] = useState("");
+    const [validEmail,setValidEmail] = useState(false);
+
+
+    const [password,setPassword] = useState("");
+    const [passwordType,setPasswordType] = useState("password");
+    const [confirmPassword,setConfirmPassword] = useState("");
+    const [passwordTypeCP,setPasswordTypeCP] = useState("password");
+    const [validConfirmPassword,setValidConfirmPassword] = useState(false);
+    const [validPassword,setValidPassword] = useState(false);
+
+    // const [isButtonDisable,setIsButtonDisable] = useState(true);
+
+
     const [userData,setUserData] = useState();
+
+
+    useEffect(()=>{
+        let b = validName(firstName);
+        setValidFirstName(b);
+    },[firstName])
+
+
+
+    useEffect(()=>{
+        let b = validName(lastName);
+        setValidLastName(b);
+        
+    },[lastName])
+
+    useEffect(()=>{
+        let b = validatePhone(phone);
+        setValidPhone(b);
+       
+    },[phone])
+
+
+    useEffect(()=>{
+        let b = validateEmail(email);
+        setValidEmail(b);
+        
+    },[email])
+
+
+    useEffect(()=>{
+        let b = validatePassword(password);
+        setValidPassword(b);
+       
+    },[password])
+
+    useEffect(()=>{
+        let b = validateConfirmPassword(password,confirmPassword);
+        setValidConfirmPassword(b);
+       
+    },[confirmPassword])
+
+
+
+    
+
+
 
     const handleFirstName = (e)=>{
         setFirstName(e.target.value);
-        let b = validName(firstName);
-        setValidNameState(b);
-        console.log({firstName});
-        console.log({b});
+        
     }
     
     const handleLastName = (e)=>{
@@ -46,6 +107,31 @@ const Form = () => {
     }
 
 
+    const togglePassword = ()=>{
+        setPasswordType(previousValue=>{
+            if(previousValue==="password"){
+                return "text";
+            }else{
+                return "password";
+            }
+        })
+        // setPasswordType("text")
+    }
+
+
+    const togglePasswordCP = ()=>{
+        setPasswordTypeCP(previousValue=>{
+            if(previousValue==="password"){
+                return "text";
+            }else{
+                return "password";
+            }
+        })
+        // setPasswordType("text")
+    }
+
+
+
     const handleSubmit=(e)=>{
         e.preventDefault();
         const userData = {
@@ -61,6 +147,12 @@ const Form = () => {
         console.log({userData});
     }
 
+
+
+
+
+
+
   return (
 
     <div className='form'>
@@ -74,14 +166,14 @@ const Form = () => {
                 </div>
                 <div className="col">
                     <div className="mb-3">
-                        <input type="text"  className={`form-control ${validNameState? 'is-valid' : ''}`} value={firstName} placeholder="First Name" onChange={(e)=>handleFirstName(e)}/>
-                        <div className="valid-feedback">Valid email address</div>
-                        <div className="invalid-feedback">Please enter a valid email address</div>
+                        <input type="text"  className={`form-control ${validFirstName? 'is-valid' : ''}`} value={firstName} placeholder="First Name" onChange={handleFirstName}/>
+                        <div className="invalid-feedback">First name should be contain atleast 3 letters</div>
                     </div>
                 </div>        
                 <div className="col">
                     <div className="mb-3">
-                        <input type="text" className="form-control"  placeholder="Last Name" onChange={(e)=>handleLastName(e)}/>
+                        <input type="text"className={`form-control ${validLastName? 'is-valid' : ''}`}  placeholder="Last Name" onChange={handleLastName}/>
+                        <div className="invalid-feedback">Last name should be contain atleast 3 letters</div>
                     </div>
                 </div>   
             </div>
@@ -93,12 +185,13 @@ const Form = () => {
                 </div>
                 <div className="col">
                     <div className="mb-3">
-                        <input type="text" className="form-control"  placeholder="Country code" onChange={(e)=>handleCountryCode(e)}/>
+                        <input type="text" className="form-control"  placeholder="Country code" onChange={handleCountryCode}/>
                     </div>
                 </div>        
                 <div className="col">
                     <div className="mb-3">
-                        <input type="text" className="form-control"  placeholder="Phone" onChange={(e)=>handlePhone(e)}/>
+                        <input type="text" className={`form-control ${validPhone? 'is-valid' : ''}`}  placeholder="Phone" onChange={handlePhone}/>
+                        <div className="invalid-feedback">Enter a valid phone number</div>
                     </div>
                 </div>   
             </div>
@@ -110,7 +203,8 @@ const Form = () => {
                 </div>
                 <div className="col">
                     <div className="mb-3">
-                        <input type="email" className="form-control"  placeholder="email address" onChange={(e)=>handleEmail(e)}/>
+                        <input type="email" className={`form-control ${validEmail? 'is-valid' : ''}`}  placeholder="email address" onChange={handleEmail}/>
+                        <div className="invalid-feedback">Please enter a valid email address</div>
                     </div>
                 </div>           
             </div>
@@ -122,12 +216,25 @@ const Form = () => {
                 </div>
                 <div className="col">
                     <div className="mb-3">
-                        <input type="password" className="form-control"  placeholder="password" onChange={(e)=>handlePassowrd(e)}/>
+                        <input type={passwordType} className={`form-control ${validPassword? 'is-valid' : ''}`}  placeholder="password" onChange={handlePassowrd}/>
+                        <div className="invalid-feedback">Password should be atleast 6 letters</div>
+                        {/* <div className="col"> */}
+                        {/* <i className="bi bi-eye-slash"></i> */}
+                            <div className="btn btn-outline-primary" onClick={togglePassword}>{ passwordType==="password"? "show" :"hide" }
+                             {/* { passwordType==="password"? <i className="bi bi-eye-slash"></i> :<i className="bi bi-eye"></i> } */}
+                            </div>
+                        {/* </div> */}
                     </div>
                 </div>  
                 <div className="col">
                     <div className="mb-3">
-                        <input type="password" className="form-control"  placeholder="confirm password" onChange={(e)=>handleConfirmPassowrd(e)}/>
+                        <input type={passwordTypeCP} className={`form-control ${validConfirmPassword? 'is-valid' : ''}`}  placeholder="confirm password" onChange={handleConfirmPassowrd}/>
+                        <div className="invalid-feedback">Password does not match</div>
+                        
+                        <div className="btn btn-outline-primary" onClick={togglePasswordCP}>{ passwordTypeCP==="password"? "show" :"hide" }
+                             {/* { passwordType==="password"? <i className="bi bi-eye-slash"></i> :<i className="bi bi-eye"></i> } */}
+                            </div>
+                       
                     </div>
                 </div>         
             </div>
@@ -135,7 +242,7 @@ const Form = () => {
              {/* Submit column */}
              <div className="row">
                 <div className="col">
-                    <button type="submit" className="btn btn-success" >Register</button>
+                    <button type="submit" className="btn btn-success" disabled={!(validFirstName && validLastName && validEmail && validPhone && validPassword && validConfirmPassword)} >Register</button>
                 </div>
                         
             </div>
--- registration-form/src/utils/Validation.js
+++ registration-form/src/utils/Validation.js
@@ -10,7 +10,28 @@ export const validName = (name) => {
     return true;
 }
 
+export const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
 
 
+  export const validatePhone = (phone) => {
+    // Remove any non-digit characters from the phone number
+    const cleanedPhone = phone.replace(/\D/g, "");
+    
+    // Check if the cleaned phone number is 10 digits long
+    const phoneRegex = /^\d{10}$/;
+    return phoneRegex.test(cleanedPhone);
+  };
 
 
+  export const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{6,}$/;
+    return passwordRegex.test(password);
+  };
+
+  export const validateConfirmPassword = (password1,password2) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{6,}$/;
+    return passwordRegex.test(password1) && passwordRegex.test(password2) && password1===password2;
+  };
